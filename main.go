c669d7defbcd3d13125f15013b9613f0964d1983
Jimmy <zinuhe@outlook.com>
2021-05-12 23:53:55 -0400

    First commit

---
+++ main.go
@@ -0,0 +1,24 @@
+// https://tutorialedge.net/golang/creating-simple-web-server-with-golang/
+
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
